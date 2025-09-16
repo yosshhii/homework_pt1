3bb52412fc74ff3e16284bb20d28e5f40ef8e071
yosshhii <bloodeyezxc@gmail.com>
2025-09-16 10:17:55 +0300

    Operation changed again

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     print("Hello Boris Ulitin! Print two integer numbers:")
     a = int(input())
     b = int(input())
-    print(a - b)
+    print(a + b)
 
 
 if __name__ == "__main__":
