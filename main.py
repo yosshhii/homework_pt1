1eb748e0fa708907370cb5711123f09be81f62be
yosshhii <bloodeyezxc@gmail.com>
2025-09-16 10:39:16 +0300

    functions added

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ def main():
     print("Hello Boris Ulitin! Print two integer numbers:")
     a = int(input())
     b = int(input())
-    print(f'Sum of numbers: {a + b}')
-    print(f'Product of numbers: {a * b}')
+    print(f'Sum of numbers: {sum(a, b)}')
+    print(f'Product of numbers: {product(a, b)}')
+    
+
+def sum(a, b):
+    return a + b
+
+
+def product(a, b):
+    return a * b
 
 
 if __name__ == "__main__":
