1e2a291ace3286c549b29327ce60be5e8eb28af3
yosshhii <bloodeyezxc@gmail.com>
2025-09-16 10:26:17 +0300

    two files changed

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     print("Hello Boris Ulitin! Print two integer numbers:")
     a = int(input())
     b = int(input())
-    print(a + b)
+    print(f'Sum of numbers: {a + b}')
+    print(f'Product of numbers: {a * b}')
 
 
 if __name__ == "__main__":
